a1972414b9098270125ef114573986f81ac72554
mancu37 <mancu37@gmail.com>
2017-08-21 01:15:03 -0300

    Mouse Orbit and objects...
    Camera Mouse Orbit Added!
    Some object into the scene Added!

---
+++ FirstPerson/Assets/Crosshair/Scripts/Crosshair.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Crosshair : MonoBehaviour {
+
+    public Camera _cam;
+    public float distance = 100f;
+
+	// Use this for initialization
+	void Start () {
+		if(_cam == null)
+        {
+            _cam = Camera.main;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 rayOrigin = _cam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayOrigin, _cam.transform.forward, out hit, distance))
+        {
+            Debug.LogFormat("Hit!: {0} {1}", hit.collider.name, hit.collider.tag);
+        }
+
+        
+
+    }
+
+
+
+}
---
+++ FirstPerson/Assets/Crosshair/Scripts/MouseOrbit.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Crosshair.Scripts
+{
+    public class MouseOrbit : MonoBehaviour
+    {
+
+        [SerializeField]
+        Vector3 cameraOffset;
+        [SerializeField]
+        float damping;
+
+        public Transform target;
+        public float distance = 5.0f;
+        public float xSpeed = 120.0f;
+        public float ySpeed = 120.0f;
+
+        public float yMinLimit = -20f;
+        public float yMaxLimit = 80f;
+
+        public float distanceMin = .5f;
+        public float distanceMax = 15f;
+
+        float x = 0.0f;
+        float y = 0.0f;
+
+
+        void Start()
+        {
+            Vector3 angles = transform.eulerAngles;
+            x = angles.y;
+            y = angles.x;
+        }
+
+        public void Update()
+        {
+
+            x += Input.GetAxis("Mouse X") * xSpeed * distance * 0.02f;
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+            y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+            Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+            Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+            //Vector3 position = rotation * negDistance + target.position;
+
+            //transform.rotation = rotation;
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, damping * Time.deltaTime);
+
+            //transform.position = position;
+        }
+
+        public static float ClampAngle(float angle, float min, float max)
+        {
+            if (angle < -360)
+                angle += 360f;
+            if (angle > 360)
+                angle -= 360f;
+            return Mathf.Clamp(angle, min, max);
+        }
+    }
+}
